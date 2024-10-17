9c0f6ac5f70cc8ef7d2f00719d1b05851c301cbf
Lahouel <hadjerlah08@gmail.com>
2024-10-17 19:19:33 +0200

    Modification du fichier HelloWorld

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Hello, World!");
+		 System.out.println("Je suis une version modifiée du code de l'exo 1");
 	}
 
 }
